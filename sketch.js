b069571b975e7deaf87bf772aa577647c39ba94b
Nuisance256 <92674122+Yoylecake256@users.noreply.github.com>
2022-02-28 14:55:02 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var edges;
+
+function preload(){
+
+}
+
+function setup(){
+    createCanvas(2732, 1250);
+
+}
+
+function draw(){
+    background("black");
+    edges = createEdgeSprites();
+drawSprites();
+}
